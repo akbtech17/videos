1a304f276008af29b0efd50d2c2ee5181c5f733b
anshul bansal <akb.tech17@gmail.com>
2021-02-19 00:58:28 +0530

    created form-input-field in SearchField

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class SearchField extends React.Component {
   render() {
-    return <div className="search-field">Search Field</div>;
+    return (
+      <div className="search-field">
+        <form>
+          <input type="text" placeholder="Search Videos..." />
+        </form>
+      </div>
+    );
   }
 }
 
