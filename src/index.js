2f6ac2d01863653e5dd3d13902c33c0254a3cd85
anshul bansal <akb.tech17@gmail.com>
2021-02-19 00:53:02 +0530

    created App and wired-up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const App = () => {
-    return <div className="App">
-        Index
-    </div>
-}
 
 ReactDOM.render(
     <App />,
