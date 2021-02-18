9962df4f6d1217617ceb34e3048f68267fe4d3dd
anshul bansal <akb.tech17@gmail.com>
2021-02-19 00:56:32 +0530

    created SearchField and wired-up

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import SearchField from "./SearchField";
 
 class App extends React.Component {
-    render() {
-        return (<div className="app">
-            App
-        </div>);
-    }
+  render() {
+    return (
+      <div className="app">
+        <SearchField />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SearchField.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SearchField extends React.Component {
+  render() {
+    return <div className="search-field">Search Field</div>;
+  }
+}
+
+export default SearchField;
