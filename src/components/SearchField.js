52a6218a17ff6491e89963051bdc821f5303a342
anshul bansal <akb.tech17@gmail.com>
2021-02-19 01:04:24 +0530

    introduced state for input-text and updated it every time val of input field got changed

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 class SearchField extends React.Component {
+  state = { inputText: "" };
 
   onInputChange = (e) => {
-    console.log(e.target.value);
+    this.setState({ inputText: e.target.value });
+    console.log(this.state.inputText);
   };
+
   render() {
     return (
       <div className="search-field">
