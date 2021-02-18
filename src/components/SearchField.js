f9b7f47be7a6ec48aca21fec8cf10c4014d20620
anshul bansal <akb.tech17@gmail.com>
2021-02-19 01:00:53 +0530

    grab input-value when search change, and print on console

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 class SearchField extends React.Component {
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  };
   render() {
     return (
       <div className="search-field">
         <form>
-          <input type="text" placeholder="Search Videos..." />
+          <input
+            type="text"
+            placeholder="Search Videos..."
+            onChange={this.onInputChange}
+          />
         </form>
       </div>
     );
