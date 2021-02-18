4396a93845bf7f39573f769145459043aa62432d
anshul bansal <akb.tech17@gmail.com>
2021-02-19 01:10:19 +0530

    on-form-submit search keyword is consoled

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class SearchField extends React.Component {
-  state = { inputText: "" };
+  constructor(props) {
+    super(props);
+    this.state = { inputText: "" };
+  }
 
   onInputChange = (e) => {
     this.setState({ inputText: e.target.value });
+    // console.log(this.state.inputText);
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
     console.log(this.state.inputText);
   };
 
   render() {
     return (
       <div className="search-field">
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <input
             type="text"
             placeholder="Search Videos..."
