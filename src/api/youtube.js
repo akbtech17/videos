96313943fd021055c692b21c785c043b4320118d
anshul bansal <akb.tech17@gmail.com>
2021-02-19 14:00:47 +0530

    created youtube api pre-configured axios instance

---
+++ src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = "AIzaSyD4BBuQhamwZI7ahj1RRuUTYc9N48I7qi0";
+
+//pre-configured axios instance, with base url
+export default axios.create({
+    baseURL="https://www.googleapis.com/youtube/v3",
+    params:{
+        part:"snippet",
+        maxResults:5,
+        key:KEY
+    }
+});
