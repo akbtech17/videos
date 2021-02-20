bbda5f206263f86abab1a70a5b4a631c9d3937ba
anshul bansal <akb.tech17@gmail.com>
2021-02-20 15:03:29 +0530

    displaying video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,6 +6,16 @@ const VideoDetail = ({ video }) => {
   }
   return (
     <div>
+      <div className="ui embed">
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
