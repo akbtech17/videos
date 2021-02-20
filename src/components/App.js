366d85946935f8b72b98a4a5c660ac80d28416f8
anshul bansal <akb.tech17@gmail.com>
2021-02-20 14:15:10 +0530

    updated selected video state

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,8 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("From the App!!", video);
+    // console.log("From the App!!", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
